07eaaf29c7cf4b6fe2c816aead2b8096367ded2b
luccamicale <luccamicale2015@gmail.com>
2023-01-02 16:23:13 -0300

    fix problems

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,12 @@
 require_relative './person'
 
 class Teacher < Person
-    def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-      super(age, name, parent_permission)
-      @specialization = specialization
-    end
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
