822a56f00f35a371542599fc721fd89e6c4286c6
luccamicale <luccamicale2015@gmail.com>
2023-01-04 16:54:07 -0300

    follow the requierements in Book file

---
+++ book.rb
@@ -0,0 +1,11 @@
+class Book 
+    attr_accessor :title
+    attr_accessor :author
+    # setter and getter title and author
+
+    def initialize(title, author)
+        # initialize @title and @author
+        @title = title
+        @author = author
+    end
+end     
