f42c253654caca0458a5d3136ac70733e469a64a
luccamicale <luccamicale2015@gmail.com>
2023-01-04 17:21:25 -0300

    add requirements in Book file

--- book.rb
+++ book.rb
@@ -1,11 +1,19 @@
 class Book 
     attr_accessor :title
     attr_accessor :author
+    attr_accessor :rentals
+
     # setter and getter title and author
 
     def initialize(title, author)
         # initialize @title and @author
         @title = title
         @author = author
+        @rentals = []
     end
-end     
+
+    def add_rental(rental)
+        @rentals.push(rental)
+        rental.book = self
+    end
+end
