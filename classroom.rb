58edbfb17b4b4e0d3cb31f65c2ce55099b156152
luccamicale <luccamicale2015@gmail.com>
2023-01-04 18:03:30 -0300

    fix linters

--- classroom.rb
+++ classroom.rb
@@ -1,21 +1,21 @@
 class Classroom
-    attr_accessor :label
-    # label instance variable 
-    attr_reader :student
-    # Setter and getter for @label
+  attr_accessor :label
+  # label instance variable
+  attr_reader :student
 
-    def initialize(label)
-        # Create the has-many/belongs-to relationship between Classroom and Student
-        @label = label
-        # Create the has-many side (a classroom has many students).
-        @student = []
-        # Create the belongs-to side (a student belongs to a classroom)
+  # Setter and getter for @label
 
-    end
+  def initialize(label)
+    # Create the has-many/belongs-to relationship between Classroom and Student
+    @label = label
+    # Create the has-many side (a classroom has many students).
+    @student = []
+    # Create the belongs-to side (a student belongs to a classroom)
+  end
 
-    def add_student(student)
-        @student.push(student)
-        student.classroom = self
-        # add the student in the class and the class has many students
-    end
+  def add_student(student)
+    @student.push(student)
+    student.classroom = self
+    # add the student in the class and the class has many students
+  end
 end
