2c23d78ff11c762a5f160a6763cfa9d7ec848343
luccamicale <luccamicale2015@gmail.com>
2023-01-09 13:49:40 -0300

    create file create_book

--- app.rb
+++ app.rb
@@ -4,46 +4,6 @@ require_relative './student'
 require_relative './teacher'
 require_relative './rental'
 
-
-
-  
-
-  def create_person
-    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
-    selection = gets.chomp.to_i
-    if [1, 2].include?(selection)
-      print 'Age: '
-      age = gets.chomp
-      print 'Name: '
-      name = gets.chomp
-      case selection
-      when 1
-        print 'Classroom: '
-        classroom = gets.chomp
-        print 'Has parent permission? [Y/N]: '
-        permission = gets.chomp
-        Student.new(age, classroom, name: name, parent_permission: permission)
-        puts 'Student created!'
-      when 2
-        print 'Specialization: '
-        specialization = gets.chomp
-        Teacher.new(age, specialization, name: name)
-        puts 'Teacher created!'
-      end
-    else
-      puts 'Wrong selection!'
-    end
-  end
-
-  def create_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    Book.new(title, author)
-    puts 'Book succesfully created!'
-  end
-
   def create_rental
     if Book.all.empty? || Person.all.empty?
       puts "There\s no persons or books added yet!"
---
+++ modules/create_book.rb
@@ -0,0 +1,8 @@
+def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    Book.new(title, author)
+    puts 'Book succesfully created!'
+  end
