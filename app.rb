3054039325b37bb834d62eced26012db1d07a73e
luccamicale <luccamicale2015@gmail.com>
2023-01-09 13:56:05 -0300

    refactor file app

--- app.rb
+++ app.rb
@@ -3,5 +3,18 @@ require_relative './person'
 require_relative './student'
 require_relative './teacher'
 require_relative './rental'
+require_relative './modules/show_books'
+require_relative './modules/show_person'
+require_relative './modules/create_person'
+require_relative './modules/create_book'
+require_relative './modules/create_rental'
+require_relative './modules/list_rental_id'
 
-
+class App
+  include ListBooks
+  include ListPerson
+  include CreatePerson
+  include CreateBook
+  include CreateRental
+  include ListRentalId
+end
