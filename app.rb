3050a5a1f11a5095bf03637e00d21be3d5497108
luccamicale <luccamicale2015@gmail.com>
2023-01-09 13:51:38 -0300

    create file list_rental

--- app.rb
+++ app.rb
@@ -5,19 +5,3 @@ require_relative './teacher'
 require_relative './rental'
 
 
-  def list_rental_id
-    if Person.all.empty? || Book.all.empty?
-      puts 'Pleas add a person and a book first!'
-    else
-      print 'ID of person: '
-      id_selected = gets.chomp.to_i
-      Rental.all.each do |rental|
-        if rental.person.id == id_selected
-          puts %(#{rental.date}, Book "#{rental.book.title}" by #{rental.book.author}, rented by #{rental.person.name})
-        else
-          puts 'There\'s no rental with that ID'
-        end
-      end
-    end
-  end
-end
