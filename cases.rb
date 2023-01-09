d4c4f5edebc40a3471ffd9202c87935a4ae88b4c
luccamicale <luccamicale2015@gmail.com>
2023-01-09 14:15:00 -0300

    modified the file cases

--- cases.rb
+++ cases.rb
@@ -1,4 +1,18 @@
-def cases(input)
+require_relative './app'
+require_relative './interface'
+class Cases
+  def initialize
+    @app = App.new
+    @interface = Interface.new
+  end
+
+  def run
+    puts @interface.interface
+    input = gets.chomp.to_i
+    option(input)
+  end
+
+  def option(input)
     selections = {
       1 => 'show_books',
       2 => 'show_people',
