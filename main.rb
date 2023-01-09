80ff29e865edfbf585b5dacb5464e0336824f734
luccamicale <luccamicale2015@gmail.com>
2023-01-09 13:52:40 -0300

    create file cases

--- main.rb
+++ main.rb
@@ -12,26 +12,5 @@ class Main
     cases(input)
   end
 
-  def cases(input)
-    selections = {
-      1 => 'show_books',
-      2 => 'show_people',
-      3 => 'create_person',
-      4 => 'create_book',
-      5 => 'create_rental',
-      6 => 'list_rental_id'
-    }
-    case input
-    when 1..6
-      @app.send(selections[input])
-      run
-    when 7
-      puts 'Thanks for using my library!'
-    else
-      puts 'Option doesn\'t exist ¯\_(ﾟ～ﾟ)_/¯, select another one!'
-      run
-    end
-  end
-end
 obj = Main.new
 obj.run
