2bdbef3a6ba7bb2737f8262f4b45dacf037c18f0
luccamicale <luccamicale2015@gmail.com>
2023-01-10 16:47:59 -0300

    fix linters in runner

--- data/runner.rb
+++ data/runner.rb
@@ -1,7 +1,7 @@
 require 'json'
 module ShowBook
   def shows
-    json_file = File.open("./data/books.json")
+    json_file = File.open('./data/books.json')
     data_from_file = File.read(json_file)
     puts data_from_file
     JSON.parse(data_from_file)
