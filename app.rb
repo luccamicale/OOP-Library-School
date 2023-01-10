96650eb360ae3b05e4fe0fa4ac63800fb5c8a0d7
luccamicale <luccamicale2015@gmail.com>
2023-01-09 23:28:24 -0300

    fix the name of modules

--- app.rb
+++ app.rb
@@ -4,15 +4,15 @@ require_relative './student'
 require_relative './teacher'
 require_relative './rental'
 require_relative './modules/show_books'
-require_relative './modules/show_person'
+require_relative './modules/show_people'
 require_relative './modules/create_person'
 require_relative './modules/create_book'
 require_relative './modules/create_rental'
 require_relative './modules/list_rental_id'
 
 class App
-  include ListBooks
-  include ListPerson
+  include ShowBook
+  include ShowPeople
   include CreatePerson
   include CreateBook
   include CreateRental
