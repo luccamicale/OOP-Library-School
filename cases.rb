dda62392cf00898d64192fef2b7753d8af79b8f7
Odette Gleen <nkargleeny@gmail.com>
2023-01-12 00:42:43 +0100

    created and initialize the various case selections

---
+++ cases.rb
@@ -0,0 +1,41 @@
+require_relative './app'
+require_relative './interface'
+require_relative './modules/create_book'
+require_relative './modules/exit'
+class Cases
+  def initialize
+    @app = App.new
+    @interface = Interface.new
+  end
+  def run
+    puts @interface.interface
+    input = gets.chomp.to_i
+    option(input)
+  end
+  def option(input)
+    selections = {
+      1 => 'show_books',
+      2 => 'show_person',
+      3 => 'create_person',
+      4 => 'create_book',
+      5 => 'create_rental',
+      6 => 'list_rental_id',
+      7 => 'save_data'
+    }
+    case input
+    when 1..6
+      @app.send(selections[input])
+      run
+    when 7
+      @app.send(selections[input])
+      puts 'Thanks for using my library!'
+    else
+      puts 'Option doesn\'t exist ¯\_(ﾟ～ﾟ)_/¯, select another one!'
+      run
+    end
+  end
+end
+
+
+
+
