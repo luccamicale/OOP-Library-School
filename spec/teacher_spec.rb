4408858d5bc5b821296b822f215dc843cd20fee2
luccamicale <luccamicale2015@gmail.com>
2023-01-12 15:06:05 -0300

    add the test to trimmer and teacher

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,1 +1,17 @@
-require './spec_helper'
+require_relative './spec_helper'
+
+describe Teacher do
+    teacher = Teacher.new('Math', 25, name: 'Camila')
+
+    it 'should have an age equals to 25' do
+        expect(teacher.age).to eq(25)
+    end
+    
+    it 'should return true if the teacher can use the services' do
+        expect(teacher.can_use_services?).to be true
+    end
+    
+    it 'should have a specialization in Math' do
+        expect(teacher.specialization).to eq('Math')
+    end
+end
