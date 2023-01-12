4908219fb694441ea136e935f9040d6e424b2dc4
luccamicale <luccamicale2015@gmail.com>
2023-01-12 14:15:34 -0300

    add test to decorator and classroom

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,1 +1,9 @@
-require './spec_helper'
+require_relative './spec_helper'
+
+describe Classroom do
+    classroom = Classroom.new('C1')
+    
+    it 'Classroom should have a attr_accessor' do
+        expect(classroom).to respond_to('label')
+    end
+end
