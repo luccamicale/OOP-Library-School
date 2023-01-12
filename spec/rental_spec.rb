8297afaaa372319de6ee47d456740726acfae532
luccamicale <luccamicale2015@gmail.com>
2023-01-12 14:50:04 -0300

    add test to rental and student

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,1 +1,17 @@
-require './spec_helper'
+require_relative './spec_helper'
+
+describe Rental do
+    rental = Rental.new('2023-01-04', 'Lucca', 'The Little Prince')
+
+    it "should have to be equal Lucca" do
+        expect(rental.book).to eq('Lucca')
+    end
+
+    it "should have to be equal The Little Prince " do
+        expect(rental.person).to eq('The Little Prince')
+    end
+
+    it "should have to be equal 2023-01-04 " do
+        expect(rental.date).to eq('2023-01-04')
+    end
+end    
